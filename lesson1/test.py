85283cde27b7b637e296e952c626f542257ab0b6
nevern0e <nevernoe@mail.ru>
2024-07-07 19:03:47 +0300

    записан 1 код

--- lesson1/test.py
+++ lesson1/test.py
@@ -1,1 +1,12 @@
+#программа выводит информацию по заданию 1 курса "Программирование на языке python"
+a = 5
+b = 3.14
+c = "Belyatskaya Evgeniya"
+b1 = True
+c1 = 1-3j
 
+print(a,type(a),id(a))
+print(b,type(b),id(b))
+print(c,type(c),id(c),sep=" !!! ")
+print(b1,type(b1),id(b1),end=" ____ ")
+print(c1,type(c1),id(c1))
